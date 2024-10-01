45e8e1cc83db01562e1f34f48885c2ebdd646d59
Deepakkumar62022 <dk5373189@gmail.com>
2024-10-01 12:31:47 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: select*from empdata
+LIMIT 0, 5000
+
+-- Date: 2024-10-01 12:20
+*/
+INSERT INTO `` (`Id`,`Name`,`Email_id`,`Phoneno`,`Address`,`Post`,`Salary`) VALUES (1,'Ashok','am5152301@gmail.com',7428312705,'Dadri','Data Analyst',60000);
